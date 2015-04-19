d3d91529f07ec6ca054140cf83afc8df417b6968
Mukesh <amukesh.mk@gmail.com>
2015-04-19 23:11:12 +0530

    Folder Node
    Creation of the folder node

--- newFM.py
+++ newFM.py
@@ -1,3 +1,42 @@
+class folder:
+	
+	def __init__(self,name = None):
+		self.name = name
+		self.parent = None
+		self.child = []
+		
+	def get_parent(self):
+		return self.parent
+	
+	def get_nth_child(self,n):
+		if (n-1) > len(self.child):
+			raise "invalid n !!"
+		else:
+			return self.child[n-1]
+	
+	def get_name(self):
+		return self.name
+		
+	def get_all_childs(self):
+		return self.child
+		
+	def set_name(self):
+		self.name = raw_input("enter name of folder:")
+	
+	def add_child(self,ch):
+		self.child.append(ch)
+		
+	def disp(self):
+		print "\n"
+		
+		print "Name:" + self.name
+		if not self.parent == None:
+			print "Parent:" + self.parent.get_name()
+		else:
+			print "parent: None"
+		for i in range(len(self.child)):
+			print "child: " + str(i+1) + " "+ self.child[i].get_name()
+
 class file_manager:
 	
 	def __init__(self):
