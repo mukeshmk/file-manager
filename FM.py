ec531eb241166268ebfe697a040a631fd8dbc07d
Mukesh <amukesh.mk@gmail.com>
2015-04-19 23:02:36 +0530

    creating the folder node
    creating the folder node and creating it.

--- FM.py
+++ FM.py
@@ -8,3 +8,23 @@ class file_manager:
 	def create_fm(self):
 		print "THE ROOT NODE !!"
 		self.root.set_name()
+	def create_folder(self):
+		temp = folder()
+		temp.set_name()
+		
+		ind = 0
+		flag = 1
+		dir = raw_input("enter name of folder to be inserted into:")
+		for i in range(len(self.lfol)):
+			if self.lfol[i].get_name() == dir:
+				fold = self.lfol[i]
+				fold.add_child(temp)
+				temp.parent = fold
+				ind = i
+				flag = 0
+				self.lfol.insert(i,temp)
+				break
+		
+		if flag:
+			print "folder not created invalid directory"
+	
