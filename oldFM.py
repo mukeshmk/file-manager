c91c24a441542d955897d1d7765ee444a600d556
Mukesh <amukesh.mk@gmail.com>
2015-04-19 23:17:13 +0530

    Addition of File
    Addition of Files to the structure

--- oldFM.py
+++ oldFM.py
@@ -72,6 +72,27 @@ class file_manager:
 	def create_fm(self):
 		print "THE ROOT NODE !!"
 		self.root.set_name()
+	def create_file(self):
+		temp = file()
+		temp.set_name()
+		
+		flag = 1
+		
+		dir = raw_input("enter name of folder to be inserted into:")
+		
+		for i in range(len(self.lfol)):
+			if self.lfol[i].get_name() == dir:
+				fold = self.lfol[i]
+				if not fold.get_left() == None:
+					break
+				fold.set_left(temp)
+				temp.parent = fold
+				flag = 0
+				self.lfile.append(temp)
+				break
+		
+		if flag:
+			print "file not created"
 	def create_folder(self):
 		temp = folder()
 		temp.set_name()
